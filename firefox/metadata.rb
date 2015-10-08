66e5a62be8cf49bb51cafe5f39d59e538904819f
root <root@C2445575914.domain>
2015-10-08 01:16:01 -0400

    add

---
+++ firefox/metadata.rb
@@ -0,0 +1,17 @@
+name 'firefox'
+maintainer 'Tim Smith'
+maintainer_email 'tsmith84@gmail.com'
+license 'Apache 2.0'
+description 'Installs Mozilla Firefox on multiple operating systems'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '2.0.5'
+
+%w(redhat centos scientific amazon oracle windows mac_os_x debian ubuntu).each do |os|
+  supports os
+end
+
+suggests 'dmg', '~> 2.2'
+suggests 'windows', '~> 1.0'
+
+source_url 'https://github.com/tas50/firefox' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/firefox/issues' if respond_to?(:issues_url)
---
+++ httpd/libraries/provider_priority_rhel.rb
@@ -0,0 +1,11 @@
+require 'chef/platform/provider_priority_map'
+require_relative 'provider_httpd_service_rhel_systemd'
+require_relative 'provider_httpd_service_rhel_sysvinit'
+
+if defined? Chef::Platform::ProviderPriorityMap
+  Chef::Platform::ProviderPriorityMap.instance.priority(
+    :httpd_service,
+    [Chef::Provider::HttpdServiceRhelSystemd,  Chef::Provider::HttpdServiceRhelSysvinit],
+    platform_family: 'rhel'
+  )
+end
